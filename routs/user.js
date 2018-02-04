31d29bed462350ceaa780672e514ce98050abfba
smrcascao <smrcascao@smrcascao-K56CB>
2018-02-04 22:11:02 +0000

    a

--- routs/user.js
+++ routs/user.js
@@ -7,6 +7,14 @@ var express = require('express');
 
 var router = express.Router();
 //connection data base
+const {Pool, Client}  = require('pg')
+const pool = new Pool({
+    database: 'quinta_da_couceira',
+    user: 'qcouceira',
+    password: 'qcouceira',
+    host: 'qcouceira.cmjmvmmgcc89.us-east-2.rds.amazonaws.com',
+    port: 5432
+})
 
 
 
@@ -17,34 +25,23 @@ var router = express.Router();
 
 // sample route with a route the way we're used to seeing it
 
-
-router.get('/', function(req, res) {
-	console.log("asdas");
-    res.send('GET handler for /dogs route.');
-});
-
-router.post('/', function(req, res) {
-    res.send('POST handler for /dogs route.');
-});
-
-
-const {Pool, Client}  = require('pg')
-const pool = new Pool({
-  database: 'quinta_da_couceira',
-  user: 'qcouceira',
-  password: 'qcouceira',
-  host: 'qcouceira.cmjmvmmgcc89.us-east-2.rds.amazonaws.com',
-  port: 5432
-})
-
 router.get('/api', (req, res) => {
 	pool.query('SELECT * from tbl_city', (err,resp)=> {
 		console.log(err,resp);
 		res.send(resp);
 		pool.end()
 	})
-	  
 });
+// ==============================================
+
+//Create New User
+router.post('/newuser', function(req, res) {
+    res.send('POST handler for /dogs route.');
+
+
+
+});
+
 
 
 module.exports = router;
